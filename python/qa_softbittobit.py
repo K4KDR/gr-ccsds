42713fa817dae4654b158137059134aa14610646
general SSIMUC user account <ssimuc@st-nav-sim3.(none)>
2014-11-25 17:26:43 +0100

    Add softbittobit block, QA tests for PSK (de)mod
    -Add new block that converts softbits to hard bits
    -Enable unittest for PSK modulator
    -Add and enable unittest for PSK detector

---
+++ python/qa_softbittobit.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2014 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+import ccsds_swig as ccsds
+
+class qa_softbittobit (gr_unittest.TestCase):
+
+    def setUp (self):
+        self.tb = gr.top_block ()
+
+    def tearDown (self):
+        self.tb = None
+
+    def test_001_t (self):
+        src_data = (0,1,-1,-0.5,0.77)
+        expected_result = (1, 1, 0, 0, 1)
+        src = blocks.vector_source_f(src_data)
+        sqr = ccsds.softbittobit()
+        dst = blocks.vector_sink_b()
+        self.tb.connect(src, sqr)
+        self.tb.connect(sqr, dst)
+        self.tb.run()
+        result_data = dst.data()
+        self.assertFloatTuplesAlmostEqual(expected_result, result_data, 6)
+
+
+if __name__ == '__main__':
+    gr_unittest.run(qa_softbittobit, "qa_softbittobit.xml")
