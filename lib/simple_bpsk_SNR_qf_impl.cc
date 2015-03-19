1e2ee1142124ac95321128e1812c130e65c4708c
Michael Wiedemann <michaelwiedemann@gmx.de>
2015-03-19 13:48:45 +0100

    simple_bpsk_SNR: 	moving samples around zero seems to work

--- lib/simple_bpsk_SNR_qf_impl.cc
+++ lib/simple_bpsk_SNR_qf_impl.cc
@@ -26,6 +26,7 @@
 #include "simple_bpsk_SNR_qf_impl.h"
 #include <volk/volk.h>
 #include <stdio.h>
+#include <boost/math/special_functions/sign.hpp>
 
 namespace gr {
   namespace ccsds {
@@ -75,38 +76,69 @@ namespace gr {
         float *out = (float *) output_items[0];
 	
 	// number of input items processed
+	size_t nii = noutput_items * d_WINDOW_SIZE;
+
+	/* always more input than output * window_size
+	/
 	size_t nii = ninput_items[0];
 	if (nii > noutput_items * d_WINDOW_SIZE)
 	{
-	nii = noutput_items * d_WINDOW_SIZE;
+		//printf("\ntoo output wanted ... reducing\n");
+		nii = noutput_items * d_WINDOW_SIZE;
 	}
+	*/
 
 	size_t align = volk_get_alignment();
-	float *real = (float *) volk_malloc(nii * sizeof(float), align);
-	float *imag = (float *) volk_malloc(nii * sizeof(float), align);
-	float *mean_real = (float *) volk_malloc(noutput_items * sizeof(float),align);
+	float *real_part = (float *) volk_malloc(nii * sizeof(float), align);
+	gr_complex *positive = (gr_complex*) volk_malloc(nii * sizeof(gr_complex), align);
+	gr_complex *mean_free = (gr_complex*) volk_malloc(d_WINDOW_SIZE * sizeof(gr_complex), align);
+	int8_t *sgn_vector = (int8_t *) volk_malloc(nii * sizeof(int8_t), align);
 	float sum_real=0;
+	float mean_real=0;
+	int8_t sgn=0;
+	const gr_complex minus_one = -1;
+	uint16_t  window_offset = 0;
 
+	const float *complexVectorPtr = (float*) in;
+	float *positiveVectorPtr = (float*) positive;
+	// flip negative real part
+	for(size_t i=0; i<nii; i++)
+	{
+		sgn = boost::math::sign(*complexVectorPtr);
+		*(sgn_vector + i) = sgn;	
+		// get the real part of the inputs (see volk_32fc_deinterleave_real_32f_generic implementation)
+		*positiveVectorPtr++ = *complexVectorPtr++ * sgn;
+		// and copy the imaginary part
+		*positiveVectorPtr++ = *complexVectorPtr++;
+	}
+	
 	// get real part of points
-	volk_32fc_deinterleave_real_32f(real,in,nii);
+	volk_32fc_deinterleave_real_32f(real_part,positive,nii);
 	
 	for (int i=0; i < noutput_items; i++)
 	{
-        volk_32f_accumulator_s32f(&sum_real,real,d_WINDOW_SIZE);
-	//(mean_real + i) = sum_real / d_WINDOW_SIZE;
-	*(out + i) = sum_real / d_WINDOW_SIZE;
+		window_offset = i * d_WINDOW_SIZE;
+        	volk_32f_accumulator_s32f(&sum_real,(real_part + window_offset), d_WINDOW_SIZE);
+		mean_real =sum_real / d_WINDOW_SIZE;
+		for(int j=0; j< d_WINDOW_SIZE;j++)
+		{
+			sgn = *(sgn_vector + window_offset + j);
+			*(mean_free +j)  = *(in + window_offset + j) - (sgn * mean_real);
+		}
 	}
-
+	
+	// debug output
+	
 	// Tell runtime system how many input items we consumed on
         // each input stream.
         consume_each (nii);
 
 	// free allocated memory
-	volk_free(mean_real);
-	volk_free(real);
-	volk_free(imag);
+	volk_free(positive);
+	volk_free(mean_free);
+	volk_free(real_part);
 
-	printf("window_size: %d\tnout: %d\tnin: %d\n", d_WINDOW_SIZE, noutput_items, nii);
+	printf("window_size: %lu\tnout: %d\tnin: %lu\tni_given: %d\n", d_WINDOW_SIZE, noutput_items, nii,ninput_items[0]);
         // Tell runtime system how many output items we produced.
         return (noutput_items);
     }
